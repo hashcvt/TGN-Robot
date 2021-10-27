d273cdcda257d7203c73cdb4dccfce4edacbd381
Trojen1 <88619598+Trojen1@users.noreply.github.com>
2021-10-27 11:33:55 +0530

    Update logomaker.py

--- TGNRobot/modules/logomaker.py
+++ TGNRobot/modules/logomaker.py
@@ -36,11 +36,11 @@ async def lego(event):
     draw.text((x, y), text, font=font, fill="black", stroke_width=25, stroke_fill="yellow")
     fname2 = "LogoByzaid.png"
     img.save(fname2, "png")
-    await tbot.send_file(event.chat_id, fname2, caption="✌️✨")
+    await tbot.send_file(event.chat_id, fname2, caption="Made by: @Yukinonthecutebot ✌💫")
     if os.path.exists(fname2):
             os.remove(fname2)
  except Exception as e:
-   await event.reply(f'Error Report @godfathersupport, {e}')
+   await event.reply(f'Error Report @waifuNetwork, {e}')
 
 
 
@@ -76,11 +76,11 @@ async def lego(event):
     draw.text((x, y), text, font=font, fill="white", stroke_width=0, stroke_fill="white")
     fname2 = "LogoByEvil.png"
     img.save(fname2, "png")
-    await tbot.send_file(event.chat_id, fname2, caption="✌️✨")
+    await tbot.send_file(event.chat_id, fname2, caption="Made by: @Yukinonthecutebot ✌💫")
     if os.path.exists(fname2):
             os.remove(fname2)
  except Exception as e:
-   await event.reply(f'Error Report @Gotfathersupport, {e}')
+   await event.reply(f'Error Report @waifuNetwork, {e}')
 
 file_help = os.path.basename(__file__)
 file_help = file_help.replace(".py", "")
@@ -91,4 +91,4 @@ __help__ = """
  ❍ /logo text :  Create your logo with your name
 
  """
-__mod_name__ = "Logo"
+__mod_name__ = "🤍 Logo 🤍"
