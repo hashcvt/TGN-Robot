4aaf0160705722f97bb8d05ff20e2cfe96a12f00
Trojen1 <88619598+Trojen1@users.noreply.github.com>
2021-10-27 12:23:10 +0530

    Update sample_config.py

--- TGNRobot/sample_config.py
+++ TGNRobot/sample_config.py
@@ -14,17 +14,17 @@ class Config(object):
     # REQUIRED
     # Login to https://my.telegram.org and fill in these slots with the details given by it
 
-    API_ID = 123456  # integer value, dont use ""
-    API_HASH = "boom"
-    TOKEN = "BOT_TOKEN"  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
-    OWNER_ID = 1669178360  # If you dont know, run the bot and do /id in your private chat with it, also an integer
-    OWNER_USERNAME = "Timesisnotwaiting" 
-    SUPPORT_CHAT = "GodfatherSupport"  # Your own group for support, do not add the @
+    API_ID = 4091096  # integer value, dont use ""
+    API_HASH = "6bb0682b4af56456201c3b9d8b99c94a"
+    TOKEN = "2025919134:AAH1EVKeu8gNiLuKSBuDGV4BRkwbfuBzbQs"  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
+    OWNER_ID = 1954364940  # If you dont know, run the bot and do /id in your private chat with it, also an integer
+    OWNER_USERNAME = "tr0j3n" 
+    SUPPORT_CHAT = "waifuNetwork"  # Your own group for support, do not add the @
     JOIN_LOGGER = (
-        -1001392103597
+        -1001568719671
     )  # Prints any new group the bot is added to, prints just the name and ID.
     EVENT_LOGS = (
-        -1001392103597
+        -1001568719671
     )  # Prints information like gbans, sudo promotes, AI enabled disable states that may help in debugging and shit
 
     # RECOMMENDED
@@ -34,7 +34,7 @@ class Config(object):
     WEBHOOK = False
     INFOPIC = True
     URL = None
-    SPAMWATCH_API = ""  # go to support.spamwat.ch to get key
+    SPAMWATCH_API = "cI1g0oI7ttUNM1VihXYOKCXsrT~kxKtJtnTJCy0UPfcg6EdjvL0g~dzYd9q2V1Y0"  # go to support.spamwat.ch to get key
     SPAMWATCH_SUPPORT_CHAT = "SpamWatchSupport"
 
     # OPTIONAL
@@ -58,9 +58,9 @@ class Config(object):
     BAN_STICKER = ""  # banhammer marie sticker id, the bot will send this sticker before banning or kicking a user in chat.
     ALLOW_EXCL = True  # Allow ! commands as well as / (Leave this to true so that blacklist can work)
     CASH_API_KEY = (
-        "awoo"  # Get your API key from https://www.alphavantage.co/support/#api-key
+        "UV7PDV3CTZ4RF6JG"  # Get your API key from https://www.alphavantage.co/support/#api-key
     )
-    TIME_API_KEY = "zaid"  # Get your API key from https://timezonedb.com/api
+    TIME_API_KEY = "6NS5U207UB3D"  # Get your API key from https://timezonedb.com/api
     WALL_API = (
         "awoo"  # For wallpapers, get one from https://wall.alphacoders.com/api.php
     )
