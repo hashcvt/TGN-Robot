d9decff4917237b355eb6697806cf2268954c277
Trojen1 <88619598+Trojen1@users.noreply.github.com>
2021-10-27 11:16:21 +0530

    Update debug.py

--- TGNRobot/modules/debug.py
+++ TGNRobot/modules/debug.py
@@ -65,7 +65,7 @@ dispatcher.add_handler(LOG_HANDLER)
 
 DEBUG_HANDLER = CommandHandler("debug", debug)
 dispatcher.add_handler(DEBUG_HANDLER)
-
-__mod_name__ = "Debug"
+ 
+__mod_name__ = "🤍 Debug 🤍"
 __command_list__ = ["debug"]
 __handlers__ = [DEBUG_HANDLER]
