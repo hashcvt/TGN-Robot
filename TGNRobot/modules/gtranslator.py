ef29fc8621a2cbb7be17c46316f8068d00dd3761
Trojen1 <88619598+Trojen1@users.noreply.github.com>
2021-10-28 06:53:00 +0530

    Update gtranslator.py

--- TGNRobot/modules/gtranslator.py
+++ TGNRobot/modules/gtranslator.py
@@ -131,6 +131,20 @@ def totranslate(update: Update, context: CallbackContext):
         update.effective_message.reply_text("The intended language is not found!")
     else:
         return
+__help__ = """
+ ❍ /tr as reply to a long message
+*Example:* 
+ ❍ /tr en*:* translates something to english
+ ❍ /tr si-en*:* translates sinhala to english
+
+*Language Codes*
+`af,am,ar,az,be,bg,bn,bs,ca,ceb,co,cs,cy,da,de,el,en,eo,es,
+et,eu,fa,fi,fr,fy,ga,gd,gl,gu,ha,haw,hi,hmn,hr,ht,hu,hy,
+id,ig,is,it,iw,ja,jw,ka,kk,km,kn,ko,ku,ky,la,lb,lo,lt,lv,mg,mi,mk,
+ml,mn,mr,ms,mt,my,ne,nl,no,ny,pa,pl,ps,pt,ro,ru,sd,si,sk,sl,
+sm,sn,so,sq,sr,st,su,sv,sw,ta,te,tg,th,tl,tr,uk,ur,uz,
+vi,xh,yi,yo,zh,zh_CN,zh_TW,zu`
+"""
 
 
 TRANSLATE_HANDLER = DisableAbleCommandHandler(["tr", "tl"], totranslate)
