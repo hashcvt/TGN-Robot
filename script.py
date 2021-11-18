a429019843c79eb7b338b566e396a84ba84d3513
Trojen1 <88619598+Trojen1@users.noreply.github.com>
2021-11-18 11:43:23 +0530

    Create script.py

---
+++ script.py
@@ -0,0 +1,19 @@
+class script(object):
+
+    START_MSG = """ <b>Hi {}
+
+This is the Image Editor Of Yukino which Supports various modes
+
+For more click help....</b>"""
+    HELP_MSG = """ Follow these Steps..
+
+<code>🌀 Send me any Image to Edit..</code>
+
+<code>🌀 Select the Corresponding mode that you need</code>
+
+<code>🌀 Your Edited Image will be Uploaded </code>
+
+
+© @tr0j3n"""
+
+
