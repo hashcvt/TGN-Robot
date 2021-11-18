e449a2e7a83c08f706c74254b6cf10bf84890e80
Trojen1 <88619598+Trojen1@users.noreply.github.com>
2021-11-18 12:27:42 +0530

    Create sample_config.py

---
+++ sample_config.py
@@ -0,0 +1,15 @@
+# By @TroJanzHEX
+
+
+import os
+
+
+class Config(object):
+    TG_BOT_TOKEN = "2025919134:AAH1EVKeu8gNiLuKSBuDGV4BRkwbfuBzbQs"
+
+    APP_ID = 4091096
+
+    API_HASH = "6bb0682b4af56456201c3b9d8b99c94a"
+
+    # Get this api from https://www.remove.bg/b/background-removal-api
+    RemoveBG_API = "YED79ZUfWQEZyMTYqkPNciff"
