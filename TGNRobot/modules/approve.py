969f0f18835eb34992294867c0e135841b78f2cb
hashcvt <95896368+hashcvt@users.noreply.github.com>
2021-12-10 09:49:56 +0530

    Update approve.py

--- TGNRobot/modules/approve.py
+++ TGNRobot/modules/approve.py
@@ -221,6 +221,6 @@ dispatcher.add_handler(APPROVAL)
 dispatcher.add_handler(UNAPPROVEALL)
 dispatcher.add_handler(UNAPPROVEALL_BTN)
 
-__mod_name__ = "🤍 Approvals 🤍"
+__mod_name__ = "❤️‍🔥 Approvals"
 __command_list__ = ["approve", "unapprove", "approved", "approval"]
 __handlers__ = [APPROVE, DISAPPROVE, APPROVED, APPROVAL]
