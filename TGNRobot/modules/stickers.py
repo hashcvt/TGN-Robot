1014328f693c06154c530409779edb43bb03f725
hashcvt <95896368+hashcvt@users.noreply.github.com>
2021-12-10 10:14:33 +0530

    Update stickers.py

--- TGNRobot/modules/stickers.py
+++ TGNRobot/modules/stickers.py
@@ -459,7 +459,7 @@ __help__ = """
  ❍ /stickers*:* Find stickers for given term on combot sticker catalogue
 """
 
-__mod_name__ = "🤍 Stickers 🤍"
+__mod_name__ = "❤️‍🔥 Stickers "
 STICKERID_HANDLER = DisableAbleCommandHandler("stickerid", stickerid)
 GETSTICKER_HANDLER = DisableAbleCommandHandler("getsticker", getsticker)
 KANG_HANDLER = DisableAbleCommandHandler("kang", kang, admin_ok=True)
