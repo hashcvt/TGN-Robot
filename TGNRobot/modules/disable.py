f971f731d04c2af2233509565525b60c57771853
hashcvt <95896368+hashcvt@users.noreply.github.com>
2021-12-10 10:08:23 +0530

    Update disable.py

--- TGNRobot/modules/disable.py
+++ TGNRobot/modules/disable.py
@@ -348,7 +348,7 @@ if is_module_loaded(FILENAME):
     ❍ /listcmds*:* list all possible toggleable commands
     """
 
-    __mod_name__ = "🤍 Disable 🤍"
+    __mod_name__ = "❤️‍🔥 Disable "
 
 else:
     DisableAbleCommandHandler = CommandHandler
